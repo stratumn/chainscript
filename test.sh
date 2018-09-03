11611d065bf022083a6ce188832b02e9bce49f8e
t-bast <bastien@stratumn.com>
2018-09-03 14:23:18 +0200

    test: refactor script

--- test.sh
+++ test.sh
@@ -6,38 +6,53 @@ mkdir -p test_samples
 docker pull stratumn/go-chainscript:latest
 docker pull stratumn/js-chainscript:latest
 
+# Mount a directory for reading/writing samples.
+MOUNT=type=bind,source="$(pwd)"/test_samples,target=/samples
+
+# Go container details.
+GO_CONTAINER=stratumn/go-chainscript:latest
+GO_CONTAINER_SAMPLE_FILE=/samples/go-samples.json
+
+# Javascript container details.
+JS_CONTAINER=stratumn/js-chainscript:latest
+JS_CONTAINER_SAMPLE_FILE=/samples/js-samples.json
+
+# Sample files (outside of the Docker context):
+GO_SAMPLE_FILE=./test_samples/go-samples.json
+JS_SAMPLE_FILE=./test_samples/js-samples.json
+
 # Generate test samples from all repositories:
-docker run --mount type=bind,source="$(pwd)"/test_samples,target=/samples stratumn/go-chainscript:latest generate /samples/go-samples.json
+docker run --mount $MOUNT $GO_CONTAINER generate $GO_CONTAINER_SAMPLE_FILE
 if [ ! $? -eq 0 ]; then
     exit 1
 fi
 
-docker run --mount type=bind,source="$(pwd)"/test_samples,target=/samples stratumn/js-chainscript:latest generate /samples/js-samples.json
+docker run --mount $MOUNT $JS_CONTAINER generate $JS_CONTAINER_SAMPLE_FILE
 if [ ! $? -eq 0 ]; then
     exit 1
 fi
 
 # Verify files:
-if [ ! -e ./test_samples/go-samples.json ]; then
+if [ ! -e $GO_SAMPLE_FILE ]; then
     echo "go-samples.json is missing"
     exit 1
 fi
 
-if [ ! -e ./test_samples/js-samples.json ]; then
+if [ ! -e $JS_SAMPLE_FILE ]; then
     echo "js-samples.json is missing"
     exit 1
 fi
 
 # Validate test samples:
 echo "---------- Javascript ----------"
-docker run --mount type=bind,source="$(pwd)"/test_samples,target=/samples stratumn/js-chainscript:latest validate /samples/go-samples.json
+docker run --mount $MOUNT $JS_CONTAINER validate $GO_CONTAINER_SAMPLE_FILE
 if [ ! $? -eq 0 ]; then
     exit 1
 fi
 echo "--------------------"
 
 echo "---------- Golang ----------"
-docker run --mount type=bind,source="$(pwd)"/test_samples,target=/samples stratumn/go-chainscript:latest validate /samples/js-samples.json
+docker run --mount $MOUNT $GO_CONTAINER validate $JS_CONTAINER_SAMPLE_FILE
 if [ ! $? -eq 0 ]; then
     exit 1
 fi
